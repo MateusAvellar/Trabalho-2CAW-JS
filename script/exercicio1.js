f46e8d8a110f2ec0052592c61fbcb5d8bd1d11b7
Mateus Avellar <alun25204708360027@h2-15.faculdade.edu>
2026-03-24 09:36:16 -0300

    Adicionando exercicio 1 e 2

---
+++ script/exercicio1.js
@@ -0,0 +1,15 @@
+function verificar() {
+
+    let numero = document.getElementById("numero").value;
+
+    if (numero > 0) {
+        document.getElementById("resultado").innerText = "O número é positivo";
+    } 
+    else if (numero < 0) {
+        document.getElementById("resultado").innerText = "O número é negativo";
+    } 
+    else {
+        document.getElementById("resultado").innerText = "O número é zero";
+    }
+
+}
---
+++ script/exercicio2.js
@@ -0,0 +1,8 @@
+function mostrarNumeros(){
+    let resultado = "";
+
+    for ( let i = 1; i <= 10; i++){
+        resultado += i + " ";
+    }
+    document.getElementById("resultado").innerText = resultado;
+}
